a64e111f1c968eebcdd5cff905a60a39b893839a
siddardha-007 <siddardhabvs@gmail.com>
2026-09-22 14:21:19 +0530

    feat(rating): add openfeign tp communicate with movie-service

--- rating-service/src/main/java/com/streambox/rating/service/RatingService.java
+++ rating-service/src/main/java/com/streambox/rating/service/RatingService.java
@@ -1,6 +1,8 @@
 package com.streambox.rating.service;
 
+import com.streambox.rating.client.MovieClient;
 import com.streambox.rating.dto.CreateRatingRequest;
+import com.streambox.rating.dto.MovieResponse;
 import com.streambox.rating.dto.RatingResponse;
 import com.streambox.rating.entity.Rating;
 import com.streambox.rating.repository.RatingRepository;
@@ -15,10 +17,16 @@ import java.util.List;
 public class RatingService {
     private final RatingRepository ratingRepository;
 
+    private final MovieClient movieClient;
+
     public RatingResponse addRating(
             Long userId,
             CreateRatingRequest request
     ) {
+
+        // 1. Check movie exists
+        MovieResponse movie =
+                movieClient.getMovieById(request.movieId());
 
         if (ratingRepository.existsByUserIdAndMovieId(
                 userId,
@@ -31,7 +39,7 @@ public class RatingService {
 
         Rating rating = Rating.builder()
                 .userId(userId)
-                .movieId(request.movieId())
+                .movieId(movie.id())
                 .score(request.score())
                 .comment(request.comment())
                 .createdAt(LocalDateTime.now())
