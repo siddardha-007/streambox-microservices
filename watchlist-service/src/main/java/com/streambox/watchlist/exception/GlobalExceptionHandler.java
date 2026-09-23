e60cefdd374c42c4e875667f8b8337a1df138f0e
siddardha-007 <siddardhabvs@gmail.com>
2026-09-23 11:04:00 +0530

    fix(watchlist): fixed exception status code issue

--- watchlist-service/src/main/java/com/streambox/watchlist/exception/GlobalExceptionHandler.java
+++ watchlist-service/src/main/java/com/streambox/watchlist/exception/GlobalExceptionHandler.java
@@ -4,8 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MovieServiceException.class)
--- watchlist-service/src/main/java/com/streambox/watchlist/security/SecurityConfig.java
+++ watchlist-service/src/main/java/com/streambox/watchlist/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.streambox.watchlist.security;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +31,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth ->
                         auth
+                                .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                                 .anyRequest()
                                 .authenticated()
                 )
